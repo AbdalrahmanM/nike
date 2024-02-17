3a7710d9cbdce058e44c7c466de20d5b42f12b9f
AbdulrahmanM <abdodj18@gmail.com>
2024-02-17 21:20:10 +0300

    Create and finish other section of subscribe,popular,quality,footer etc.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { CustomerReviews,Footer,Hero,PopularProducts,Services,SpecialOffer,Subsc
 const App = () =>(
   <main className="relative">
     <Nav/>
-    <section className="xl:padding-1 wide:padding-r padding-b">
+    <section className="xl:padding-1 wide:padding-r padding-b md:pl-8">
       <Hero/>
     </section>
     <section className="padding"> 
